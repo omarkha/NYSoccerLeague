3026d59c467bb068432046dea2629a553fc90f09
Omar Khalil <omareldagestany@outlook.com>
2022-05-22 20:12:54 -0400

    lets see

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config() // Add this line
 
-let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/test'
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/Cluster0'
 
 mongoose
   .connect(dbUrl)
