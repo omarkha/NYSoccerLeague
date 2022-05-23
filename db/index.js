e9b29cec52a77a5612520e6ce28955438336069d
Omar Khalil <omareldagestany@outlook.com>
2022-05-22 23:10:29 -0400

    lets see

--- db/index.js
+++ db/index.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
+import { connect, set, connection } from 'mongoose';
+
 require('dotenv').config() // Add this line
 
 let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/test'
 
-mongoose
-  .connect(dbUrl)
+connect(dbUrl)
   .then(() => {
     console.log('Successfully connected to MongoDB!')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
-mongoose.set('debug', true)
-const db = mongoose.connection
+set('debug', true)
+const db = connection;
 
-module.exports = db
+export default db;
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
 const clubController = require('./controllers/clubControllers.js');
 const { League, Club, Player } = require('./models');
 const morgan = require('morgan');
 const res = require('express/lib/response');
-const db = require('./db')
 const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-require('dotenv').config() 
 
 __dirname = path.resolve();
 
@@ -22,6 +21,24 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/client/build`))
 app.use(express.urlencoded({ extended: false }))
+
+
+
+require('dotenv').config() // Add this line
+
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/test'
+
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log('Successfully connected to MongoDB!')
+  })
+  .catch((e) => {
+    console.error('Connection error', e.message)
+  })
+mongoose.set('debug', true)
+const db = mongoose.connection;
+
 
 
 // await Leagie.find
