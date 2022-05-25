d908cfea01b1bc019cec72fb70ee0f72831d819a
Omar Khalil <omareldagestany@outlook.com>
2022-05-24 21:23:16 -0400

    testing

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -8,8 +8,10 @@ const Player = (props) => {
             <p>{props.club} <br/>
             {props.foot} Footed | {props.position} | {props.age} y/o | {props.height} <br/>
             {props.weight} | {props.county} | {props.city} <br/>
+            <span>
             {props.email} <br/>
-            {props.phone}</p>
+            {props.phone}</span></p>
+
             <button onClick={(e) => props.handleModify(props.firstname, props.lastname, props.position, props.club, props.foot, props.county, props.city, props.age, props.height, props.weight, props.email, props.phone, props.id)}>Modify</button>
             <button onClick={(e) => props.handleDelete(props.id)}>Remove</button>
         </div>
