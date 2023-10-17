3fec9ae352e26a63ca906675e3200d96c857b2b5
Copyres <copyresearcher@gmail.com>
2023-10-17 02:25:04 -0400

    asd

--- models/club.model.js
+++ models/club.model.js
@@ -1,22 +1,20 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const clubSchema = new Schema({
-    name: {type: String, required: true, unique: true},
-    county: {type: String, required: true},
-    city: {type: String, required: true, unique: true},
-    league: {type: String, ref: "League"},
-    img_url: {type: String, required: true},
-    email: {type: String, required: true},
-    phone: {type: String, required: true},
-    
+const clubSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    county: { type: String, required: true },
+    city: { type: String, required: true, unique: true },
+    league: { type: String, ref: "League" },
+    img_url: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-}, {timestamps: true});
-s
-const Club = mongoose.model('Club', clubSchema);
-
-
+const Club = mongoose.model("Club", clubSchema);
 
 module.exports = Club;
